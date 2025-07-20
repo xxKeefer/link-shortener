58b70fb4a3e0adab26d0ff45682f54c2848c7aa8
xxKeefer <xxkeefer.code@gmail.com>
2025-07-20 17:37:55 +1000

    feat: :tada: start the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main>
+      <h1 className="text-4xl">Test</h1>
+      <p className="font-display">Test 1 2 3</p>
+      <p className="font-emoji">🧙‍♂️</p>
+      <p className="font-sans">Lorem Ipsum</p>
+      <p className="font-sans font-black">Lorem Ipsum Fat</p>
+      <p className="font-sans font-light">Lorem Ipsum thin</p>
+      <p className="font-mono">Arigato Mr. Roboto</p>
+    </main>
+  )
+}
+
+export default App
